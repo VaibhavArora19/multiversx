7397c2bb86828b4c275abf487a206007657323bf
Vaibhav Arora <aroravaibhav817@gmail.com>
2025-03-01 13:13:55 +0530

    added lending apis

---
+++ frontend/app/api/lend/route.ts
@@ -0,0 +1,86 @@
+// import { UserSecretKey, UserSigner } from "@multiversx/sdk-core/out";
+
+import { controllerAddress, egldMoneyMarketAddress } from "@/constants";
+import { Address, AddressValue, ApiNetworkProvider, QueryRunnerAdapter, SmartContractQueriesController } from "@multiversx/sdk-core/out";
+
+const getLendingPositions = async () => {
+  const address = "erd13gh9ecruu4kg2r76ts8w64jzk2q8etxcev3w32j788fjpfg2kk0qjw8d20";
+
+  const unformattedAddress = Address.fromBech32(address);
+
+  const egldAddress = Address.fromBech32(egldMoneyMarketAddress);
+
+  const apiProv = new ApiNetworkProvider("https://devnet-api.multiversx.com");
+
+  const queryRunner = new QueryRunnerAdapter({
+    networkProvider: apiProv,
+  });
+
+  const controller = new SmartContractQueriesController({
+    queryRunner,
+  });
+
+  const query = controller.createQuery({
+    contract: controllerAddress,
+    function: "getAccountTokens",
+    arguments: [new AddressValue(egldAddress), new AddressValue(unformattedAddress)],
+  });
+
+  const response = await controller.runQuery(query);
+
+  console.log("response is: ", response);
+
+  const [tokens] = controller.parseQueryResponse(response);
+
+  console.log("getaccounttokens: ", parseInt(Buffer.from(tokens).toString("hex"), 16)); //!the amount of hegld tokens you have deposited, divide it by 46.66 after parsing it with 8 decimals and you will get the amount of egld
+
+  return parseInt(Buffer.from(tokens).toString("hex"), 16);
+};
+
+const getBorrowingPositions = async () => {
+  const address = "erd13gh9ecruu4kg2r76ts8w64jzk2q8etxcev3w32j788fjpfg2kk0qjw8d20";
+
+  const unformattedAddress = Address.fromBech32(address);
+
+  const egldAddress = Address.fromBech32(egldMoneyMarketAddress);
+
+  const apiProv = new ApiNetworkProvider("https://devnet-api.multiversx.com");
+
+  const queryRunner = new QueryRunnerAdapter({
+    networkProvider: apiProv,
+  });
+
+  const controller = new SmartContractQueriesController({
+    queryRunner,
+  });
+
+  const query = controller.createQuery({
+    contract: egldMoneyMarketAddress, //!this should be dynamic for different contracts
+    function: "getStoredAccountBorrowAmount",
+    arguments: [new AddressValue(unformattedAddress)],
+  });
+
+  const response = await controller.runQuery(query);
+
+  console.log("response is: ", response);
+
+  const [tokens] = controller.parseQueryResponse(response);
+
+  console.log("getaccounttokens: ", parseInt(Buffer.from(tokens).toString("hex"), 16)); //!the amount of hegld tokens you have deposited, divide it by 46.66 after parsing it with 8 decimals and you will get the amount of egld
+
+  return parseInt(Buffer.from(tokens).toString("hex"), 16);
+};
+
+export async function GET() {
+  const lendingPositionAmount = await getLendingPositions();
+
+  const borrowingPositionAmount = await getBorrowingPositions();
+
+  return Response.json({
+    status: 200,
+    data: {
+      lendingPositionAmount,
+      borrowingPositionAmount,
+    },
+  });
+}
--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,1 +1,5 @@
 export const ELIZA_ADDRESS = "http://localhost:3001";
+
+export const controllerAddress = "erd1qqqqqqqqqqqqqpgqwf9drqmy5wxfj36mehus7979wl659p6gv5ysu7ktra";
+export const egldMoneyMarketAddress = "erd1qqqqqqqqqqqqqpgq2udp46dvs4cvp4urak39t2fqxp7t3lpzv5ysec452j";
+export const taoMoneyMarketAddress = "erd1qqqqqqqqqqqqqpgqara7qx6funfum8jy30fctvre23rffxw4v5ysnzmlnt";
